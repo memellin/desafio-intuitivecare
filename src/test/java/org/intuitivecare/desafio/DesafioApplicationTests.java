f7f2eeb7dc726c05e50c4edb850592f2a8ac293e
Enock <enocksm@outlook.com>
2026-01-31 20:48:58 -0300

    iniciando projeto

---
+++ src/test/java/org/intuitivecare/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intuitivecare.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
